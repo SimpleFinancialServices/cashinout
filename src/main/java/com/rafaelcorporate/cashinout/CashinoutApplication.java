2ee31bf50317605eded3def348d9e110e2bb1e11
Rafael Mendes <rafaom260@yahoo.com.br>
2018-11-01 23:22:59 -0300

    Spring empty project

---
+++ src/main/java/com/rafaelcorporate/cashinout/CashinoutApplication.java
@@ -0,0 +1,12 @@
+package com.rafaelcorporate.cashinout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashinoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashinoutApplication.class, args);
+    }
+}
